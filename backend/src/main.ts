c3ff36ae26f7d3f4b7b524ceadbba853f694045d
cam96stanley <cam96stanley@gmail.com>
2025-09-28 02:44:18 -0400

    setup tailwind and changed cors port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   app.useGlobalGuards(new GlobalAuthGuard(reflector, cognitoAuthGuard));
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:5173'],
     credentials: true,
   });
 
