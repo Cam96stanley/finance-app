55e0343070ec04d5ec18dcfb126c7919c998fa35
cam96stanley <cam96stanley@gmail.com>
2025-09-27 22:52:04 -0400

    updated create transaction to update the corresponding budget category if it exists

--- backend/src/transaction/transaction.service.ts
+++ backend/src/transaction/transaction.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTransactionDto } from './dto';
 
@@ -13,7 +13,46 @@ export class TransactionService {
 
     if (!user) throw new Error('No user found');
 
-    return this.prisma.transaction.create({
+    const budget = await this.prisma.budget.findFirst({
+      where: {
+        userId: user.id,
+        categoryId: dto.categoryId,
+      },
+    });
+
+    if (budget) {
+      if (budget.maxSpending < dto.amount) {
+        throw new BadRequestException(
+          'Insufficient budget for this transaction',
+        );
+      }
+
+      const [updatedBudget, transaction] = await this.prisma.$transaction([
+        this.prisma.budget.update({
+          where: { id: budget.id },
+          data: {
+            maxSpending: { decrement: dto.amount },
+          },
+        }),
+        this.prisma.transaction.create({
+          data: {
+            amount: dto.amount,
+            senderId: user.id,
+            recipientId: dto.recipientId,
+            categoryId: dto.categoryId,
+          },
+          include: {
+            sender: { select: { name: true } },
+            recipient: { select: { name: true } },
+            category: { select: { name: true } },
+          },
+        }),
+      ]);
+
+      return { transaction, updatedBudget };
+    }
+
+    const transaction = await this.prisma.transaction.create({
       data: {
         amount: dto.amount,
         senderId: user.id,
@@ -21,9 +60,12 @@ export class TransactionService {
         categoryId: dto.categoryId,
       },
       include: {
+        sender: { select: { name: true } },
         recipient: { select: { name: true } },
         category: { select: { name: true } },
       },
     });
+
+    return { transaction };
   }
 }
